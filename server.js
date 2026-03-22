7ae3c56af5edbe44e70d78306765577b89493435
amanbhambhani <aman@digitalrubix.com>
2026-03-22 17:32:00 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+echo "# server.js" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/amanbhambhani/server.js.git
+git push -u origin main
